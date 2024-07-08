90f84bc325ceae1342f4c14ba909e829dd92fbc6
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-08 09:46:53 +0700

    operator aritmatik

---
+++ 09-operatorArtimatik.go
@@ -0,0 +1,37 @@
+package main
+
+func operatorAritmatik() {
+	// Operator Aritmatik
+	// Operator aritmatik adalah operator yang digunakan untuk melakukan operasi matematika
+	// seperti penjumlahan, pengurangan, perkalian, pembagian, dan lain-lain.
+
+	// Penjumlahan
+	var a = 10
+	var b = 10
+	var c = a + b
+	println(c)
+
+	// Pengurangan
+	var d = 10
+	var e = 10
+	var f = d - e
+	println(f)
+
+	// Perkalian
+	var g = 10
+	var h = 10
+	var i = g * h
+	println(i)
+
+	// Pembagian
+	var j = 10
+	var k = 10
+	var l = j / k
+	println(l)
+
+	// Modulus
+	var m = 10
+	var n = 10
+	var o = m % n
+	println(o)
+}
