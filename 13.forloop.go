a82b9a7335d06a92a569c6d66ace03ca7f8658de
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-08 11:12:41 +0700

    forLoop

---
+++ 13.forloop.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func forLoop() {
+	// For loop
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	// For loop with condition
+	j := 0
+	for j < 5 {
+		fmt.Println(j)
+		j++
+	}
+
+}
