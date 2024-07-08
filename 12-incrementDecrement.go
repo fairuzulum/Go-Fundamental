a82d64ec8fec2734d082f336ba2f9f0c982f7781
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-08 10:12:29 +0700

    indrement dan decrement

---
+++ 12-incrementDecrement.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// increment dan decrement
+
+	// increment
+	var i = 10
+	i = i + 1
+	fmt.Println(i)
+
+	// decrement
+	i = i - 1
+	fmt.Println(i)
+
+	// increment
+	i++
+	fmt.Println(i)
+
+	// decrement
+	i--
+	fmt.Println(i)
+}
