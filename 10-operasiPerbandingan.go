701e686a3b1772a2662dc056226f39637dd7c719
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-08 09:50:33 +0700

    operasi perbandingan

---
+++ 10-operasiPerbandingan.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Operasi Perbandingan
+	// Operator perbandingan adalah operator yang digunakan untuk membandingkan dua buah nilai.
+	// Hasil dari operasi perbandingan adalah boolean (true atau false).
+
+	// Sama dengan (==)
+	var a = 10
+	var b = 10
+	var c = a == b
+	fmt.Println(c)
+
+	name1 := "fairuz"
+	name2 := "Fairuz"
+	fmt.Println(name1 == name2) // false
+
+	
+
+}
