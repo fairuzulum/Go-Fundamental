eb04214601bf779fbe840951de9c48df2a1863f5
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-08 23:47:10 +0700

    comment

---
+++ 32-komentar.go
@@ -0,0 +1,22 @@
+// komentar adalah penjelasan yang ditulis di dalam kode program
+
+// Komentar di Go dibagi menjadi dua, yaitu komentar satu baris dan komentar multiline.
+
+/**
+ * Ini adalah komentar multiline
+ * Komentar ini bisa digunakan untuk memberikan penjelasan yang lebih panjang
+*/
+
+package main
+
+func coment() {
+	// Komentar satu baris
+	// Komentar ini hanya bisa digunakan untuk satu baris saja
+	// Komentar ini tidak akan dijalankan oleh program
+	// fmt.Println("Hello World")
+
+	// Komentar multiline
+	// Komentar ini bisa digunakan untuk beberapa baris
+	// Komentar ini tidak akan dijalankan oleh program
+	// fmt.Println("Hello World")
+}
