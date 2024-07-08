8d305a08c3f18d599b74cee386dd85e674523877
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-08 16:48:22 +0700

    function value

---
+++ 22-functionValue.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func getGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
+// func main() {
+// 	// function value => first class citizen merupakan
+// 	// kemampuan function di Go yang bisa disimpan dalam variabel,
+// 	// dikirim sebagai parameter, dan dikembalikan sebagai nilai
+
+// 	goodBye := getGoodBye
+// 	result := goodBye("Fairuz")
+// 	fmt.Println(result)
+
+// }
