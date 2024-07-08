f5d41f1e329953f453a017ff58dfd7bc2442a515
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-08 08:57:18 +0700

    tipe data number

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func helloWorld() {
 	// Hello World
 	fmt.Println("Hello World")
 }
---
+++ typeDataNumber.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Tipe Data Number
+	var age int = 20
+	fmt.Println(age)
+
+	// integer
+	// int8 => -128 to 127
+	// int16 => -32768 to 32767
+	// int32 => -2147483648 to 2147483647
+	// int64 => -9223372036854775808 to 9223372036854775807
+
+	// unsigned integer
+	// uint8 => 0 to 255
+	// uint16 => 0 to 65535
+	// uint32 => 0 to 4294967295
+	// uint64 => 0 to 18446744073709551615
+
+	// floating point
+	// float32 => 1.18e-38 to 3.4e38
+	// float64 => 2.23e-308 to 1.8e308
+
+	// complex
+	// complex64 => 32-bit real and imaginary parts
+	// complex128 => 64-bit real and imaginary parts
+
+	// Alias
+	// byte => uint8
+	// rune => int32
+
+}
