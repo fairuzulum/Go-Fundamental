b6e6c0c99ec18e9cbf3e5addc17faa1dc5365f14
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-09 15:29:14 +0700

    OS PACKAGE

--- 41-os.go
+++ 41-os.go
@@ -12,4 +12,18 @@ func main() {
 	fmt.Println("-: ", args[1])
 	fmt.Println("-: ", args[2])
 
+	// mengambil detail nama hostname
+	name, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("hostName: ", name)
+	}
+
+	// mengambil env
+	username := os.Getenv("APP_USERNAME")
+	password := os.Getenv("APP_PASSWORD")
+	fmt.Println("username: ", username)
+	fmt.Println("password: ", password)
+
 }
